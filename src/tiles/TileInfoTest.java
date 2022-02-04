9b6d50aca10bc2c0b3a327e2c556219af0161f3b
Susanne Vikström <Susanny@live.se>
2022-02-04 12:06:06 +0100

    Update TileInfoTest.java
    La till ny testmetod

--- src/tiles/TileInfoTest.java
+++ src/tiles/TileInfoTest.java
@@ -1,20 +1,28 @@
 package tiles;
 
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class TileInfoTest {
 
     @Test
-    void getTitleLowBoarder() {
+    void getTitleLowBound() {
         TileInfo tileInfo = new TileInfo();
         assertEquals("Go", tileInfo.getTitle(0));
     }
 
     @Test
-    void getTitleNull() {
+    void getTitleUppBound() {
         TileInfo tileInfo = new TileInfo();
         assertEquals("40", tileInfo.getTitle(39));
     }
+
+    /*
+    @Test
+    void getTitleTooBig() {
+        TileInfo tileInfo = new TileInfo();
+        assertEquals("40", tileInfo.getTitle(40));
+    }
+    
+     */
 }
